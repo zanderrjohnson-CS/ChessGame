Fixed data set error. added column for illegal move masking. training loop is taking VERY long.

z_chess.py
@@ -16,7 +16,7 @@ from torch.utils.data import Dataset, DataLoader
 # ===============================
 # CONFIG
 # ===============================
-COLLECT_DATA = False        # <-- SET TRUE ONLY WHEN YOU WANT TO RE-FETCH GAMES
+COLLECT_DATA = False       # <-- SET TRUE ONLY WHEN YOU WANT TO RE-FETCH GAMES
 DATASET_PATH = "chess_policy_2024_elite.npz"
 
 